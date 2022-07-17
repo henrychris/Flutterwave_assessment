13450d98bb6924985e150e2cf20567e55be92e24
Henry Ihenacho <henrychris2003@gmail.com>
2022-07-17 20:32:55 +0100

    update port no to 5000

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => { res.send("Hello World!") });
 app.post('/split-payments/compute', computeController.compute)
 
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port);
